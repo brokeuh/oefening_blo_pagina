2b40b40a07b9e70f0a2387607ea94adc2c8b0775
mobapp09 <jochem_petit@msn.com>
2019-12-12 16:15:07 +0100

    added delete button html and css layout aangepast talen toegevoegd voor delete , placeholders ect

--- src/main/java/petit/jochem/demo/model/BlogDAO.java
+++ src/main/java/petit/jochem/demo/model/BlogDAO.java
@@ -1,5 +1,6 @@
 package petit.jochem.demo.model;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BlogDAO extends CrudRepository<Blog, Integer> {
